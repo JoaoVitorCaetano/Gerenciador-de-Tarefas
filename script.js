076c72f2c58fd92031438d4892760297b2e3e173
Joao Vitor <joaovitorcaetano2808@gmail.com>
2025-03-19 11:37:45 -0300

    add-data-dificuldade

--- script.js
+++ script.js
@@ -1,34 +1,103 @@
-// seleção de elementos
+
 const taskInput = document.querySelector("#txt1");
 const taskList = document.querySelector(".task-container")
+const addTaskBtn = document.querySelector("#addTaskBtn")
+const nameTask = document.querySelector("#nameTask")
+const popup = document.querySelector(".popup")
+const select = document.querySelector("#dificuldade")
+const taskDateInput = document.querySelector("#taskDate")
+const Data = document.querySelector(".currentDate")
+
+const date = new Date()
+const dataAtual = date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+})
+Data.innerHTML = `Data Atual: ${dataAtual}`
 
 function salvarTask() {
-
+    
     if (taskInput.value.length == 0) {
-        alert("[Erro]Digite alguma tarefa")
+        alert("[Erro] Digite alguma tarefa");
+        return;
     } else {
-            const tasks = document.createElement("div")
-            tasks.classList.add("tasks")
+        
+        const tasks = document.createElement("div");
+        tasks.classList.add("tasks");
 
-            const taskTitle = document.createElement("h3")
-            taskTitle.innerText = taskInput.value
-            tasks.appendChild(taskTitle)
+        
+        const taskTitle = document.createElement("h3");
+        taskTitle.innerText = taskInput.value;
+        tasks.appendChild(taskTitle);
 
-            const doneBtn = document.createElement("button")
-            doneBtn.classList.add("finish-task")
-            doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-            tasks.appendChild(doneBtn)
 
-            const deleteBtn = document.createElement("button")
-            deleteBtn.classList.add("remove-task")
-            deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-            tasks.appendChild(deleteBtn)
+        const selectedDate = new Date(taskDateInput.value +  "T00:00:00")
+        const dataFormatada = selectedDate.toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        })
+        
+        const dataAtual = new Date();
+        dataAtual.setHours(0, 0, 0, 0) 
 
-            taskList.appendChild(tasks)
+        if (selectedDate < date) {
+            alert("Essa data já passou")
+            return;
+        } else {
+            if (selectedDate) {
+                const dateSpan = document.createElement("p") 
+                dateSpan.classList.add("date-badge")
+    
+                dateSpan.innerText = dataFormatada 
+                taskTitle.appendChild(dateSpan)
+            }
+        }
 
-            taskInput.value = ''
+        
+        const selectedDifficulty = select.value;
+
+        if (selectedDifficulty) {
+            const difficultySpan = document.createElement("span"); 
+            difficultySpan.classList.add("difficulty-badge");
+
+            
+            if (selectedDifficulty === "fácil") {  
+                difficultySpan.classList.add("difficulty-easy");
+            } else if (selectedDifficulty === "médio") {
+                difficultySpan.classList.add("difficulty-medium");
+            } else if (selectedDifficulty === "difícil") {
+                difficultySpan.classList.add("difficulty-hard");
+            }
+
+            difficultySpan.innerText = selectedDifficulty; 
+            tasks.appendChild(difficultySpan);
+        }
+
+
+        const doneBtn = document.createElement("button");
+        doneBtn.classList.add("finish-task");
+        doneBtn.innerHTML = '<i class="fa-solid fa-check" id="icon"></i>';
+        tasks.appendChild(doneBtn);
+
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("remove-task");
+        deleteBtn.innerHTML = '<i class="fa-solid fa-xmark" id="icon"></i>';
+        tasks.appendChild(deleteBtn);
+
+        
+        taskList.appendChild(tasks);
+
+        
+        taskInput.value = "";
+        select.value = "";
+        taskDateInput.value = ""
+
+        
+        popup.style.display = "none";
     }
-
 }
 
 document.addEventListener('click', (e) => {
@@ -44,4 +113,16 @@ document.addEventListener('click', (e) => {
         paiDoElemento.classList.toggle("done");
         console.log('done')
     }
+})
+
+addTaskBtn.addEventListener('click', function() {
+    
+    if (taskInput.value.length == 0) {
+        alert("[Erro] Digite alguma tarefa");
+        return;
+    } else {
+        popup.style.display = "flex"
+        nameTask.innerText = taskInput.value
+    }
+
 })
